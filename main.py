ccef343d567a5dd2d22398fdc430f0f48302a1e1
Alex Nørgaard <umbra@abstractumbra.dev>
2025-10-22 23:25:53 +0100

    remove debug prints

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ logging.basicConfig(level=logging.INFO)
 def main() -> None:
     file = pathlib.Path("test_files/SaveFile.txt")
     with Save.from_path(file) as save:
-        print(save.level)
         save.level = 100
-        print(save.level)
         save.unlock_equipment(tier=3)
 
 
